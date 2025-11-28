2c36f923081f65c0169faa0e9be185239e1d0c51
Bernal1999 <bernaldarwin@gmail.com>
2025-11-28 08:46:05 +0530

    Update models.py

--- naleh/models.py
+++ naleh/models.py
@@ -7,11 +7,13 @@ class Product (models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     image = models.ImageField(upload_to='products/',blank=True,null=True)
-
+    image2 = models.ImageField(upload_to='products/', blank=True, null=True)      # Extra image 1
+    image3 = models.ImageField(upload_to='products/', blank=True, null=True) 
     def __str__(self):
         return self.title
 
 class NewProducts(models.Model):
     image = models.ImageField(upload_to='producs/',blank=True,null=True)
     title = models.CharField(max_length=200,blank=True,null=True)
+
     description = models.CharField(max_length=300,blank=True,null=True)
